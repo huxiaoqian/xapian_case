62d9b25210f19dde9fa658421eab1cbfe0f23d34
taozhiiq <760782665@qq.com>
2014-08-14 19:53:17 +0800

    little fix(ljh)

--- zmq_index/consts.py
+++ zmq_index/consts.py
@@ -8,25 +8,25 @@ XAPIAN_INDEX_LOCK_FILE = '/tmp/xapian_weibo'
 REDIS_CONF_MAX_DB_NO = 16
 XAPIAN_SEARCH_DEFAULT_SCHEMA_VERSION = 2
 XAPIAN_REMOTE_OPEN_TIMEOUT = 300000  # 300s
-XAPIAN_ZMQ_VENT_HOST = '192.168.1.3' # vent and control host
+XAPIAN_ZMQ_VENT_HOST = '219.224.135.48' # vent and control host
 XAPIAN_ZMQ_VENT_PORT = 5580 # vent port
 XAPIAN_ZMQ_CTRL_VENT_PORT = 5581 # control port
 XAPIAN_DB_PATH = 'master_timeline_weibo_csv'
-XAPIAN_DATA_DIR = '/home/ubuntu3/huxiaoqian/case/20140724/'
-XAPIAN_STUB_FILE_DIR = '/home/ubuntu3/huxiaoqian/case/stub/'
+XAPIAN_DATA_DIR = '/home/ubuntu3/ljh/xapian_case/data/'
+XAPIAN_STUB_FILE_DIR = '/home/ubuntu3/ljh/xapian_case/stub/'
 XAPIAN_ZMQ_POLL_TIMEOUT = 100000 # 10s
 XAPIAN_FLUSH_DB_SIZE = 10000 # xapian flush db size
 XAPIAN_EXTRA_FIELD = 'sentiment' # xapian extra field
 
 # file vent queue
-VENT_REDIS_HOST = '192.168.1.3'
+VENT_REDIS_HOST = '192.168.1.4'
 VENT_REDIS_PORT = 6379
 GLOBAL_CSV_FILES = 'global_vent_queue:index'
 
 # csv data source
 FROM_CSV = 1
 if FROM_CSV:
-    CSV_FILEPATH = '/home/ubuntu3/huxiaoqian/case/csv_flow1/'
+    CSV_FILEPATH = '/home/mirage/dev/original_data/csv/flow/'
 
 def _default_redis(host=VENT_REDIS_HOST, port=VENT_REDIS_PORT, db=0):
     return redis.StrictRedis(host, port, db)
--- zmq_topic_workspace/config.py
+++ zmq_topic_workspace/config.py
@@ -4,7 +4,7 @@
 XAPIAN_ZMQ_POLL_TIMEOUT = 10000  # 10s
 
 # xapian zmq vent port
-XAPIAN_ZMQ_VENT_HOST = '192.168.1.3'  # vent ip
+XAPIAN_ZMQ_VENT_HOST = '219.224.135.46'  # vent ip
 XAPIAN_ZMQ_VENT_PORT = 5573 # vent port
 FROM_CSV = 1 # if read from csv
 
@@ -15,16 +15,16 @@ if FROM_CSV:
         return csv_input
 
     # simulate outside csv files path
-    CSV_INPUT_FILEPATH = '/mnt/ds600/share/weibo_201309/csv/20130902/'  # if folder need /, file don't
+    CSV_INPUT_FILEPATH = '/home/mirage/dev/original_data/csv/20130911_cut/'  # if folder need /, file don't
 
 # redis queue for flow vent
 GLOBAL_CSV_QUEUE_INDEX = 'global_vent_queue:index'
 GLOBAL_CSV_QUEUE_SENTIMENT = 'global_vent_queue:sentiment'
 GLOBAL_CSV_QUEUE_PROFILE = 'global_vent_queue:profile'
 GLOBAL_CSV_QUEUE_HBASE = 'global_vent_queue:hbase'
-VENT_REDIS_HOST = '192.168.1.2'
+VENT_REDIS_HOST = '192.168.1.4'
 VENT_REDIS_PORT = 6379
 
 # csv flow path where to write csv
 CHUNK_SIZE = 10000 # file chunk size
-CSV_FLOW_PATH = '/mnt/ds600/share/weibo_201309/csv/flow/'
+CSV_FLOW_PATH = '/home/mirage/dev/original_data/csv/flow/'
