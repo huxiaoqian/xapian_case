559ca7e10eda76467cd97caf3491a6a4908275cf
taozhqi <760782665@qq.com>
2014-08-20 02:04:33 +0800

    little fix(ljh)

--- consts.py
+++ consts.py
@@ -20,6 +20,7 @@ ZMQ_VENT_PORT = 5573 # vent port
 XAPIAN_ZMQ_VENT_HOST = '219.224.135.48' # vent and control host
 XAPIAN_ZMQ_VENT_PORT = 5580 # vent port
 XAPIAN_ZMQ_CTRL_VENT_PORT = 5581 # control port
+XAPIAN_ZMQ_SYNC_VENT_PORT = 5582 # sync port
 
 FROM_CSV = 1 # if read from csv
 if FROM_CSV:
